2669dbae4728f03e6ea567d4997e9711a30d92ba
unknown <wangzhi02@cn.net.ntes>
2018-05-04 16:27:35 +0800

    add city-detail

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul class="list">
+    <li class="item" v-for="(item, key) of cities" :key="key">{{ key }}</li>   
+  </ul>
+</template>
+
+<script>
+
+export default {
+    name: 'CityAlphabet',
+    props: {
+        cities: Object
+    },
+    components: {
+       
+    },
+    data () {
+      return {
+        
+      }
+    },
+    methods: {
+     
+    }
+    
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+    .list 
+        display: flex
+        flex-direction: column 
+        justify-content: center
+        position: absolute 
+        top: 1.58rem
+        right: 0
+        bottom: 0
+        width: .4rem
+        list-style: none
+        .item 
+            line-height: .4rem
+            text-align: center 
+            color: $bgColor
+</style>
+
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="search">
+      <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CitySearch',
+    components: {
+       
+    },
+    data () {
+      return {
+        
+      }
+    },
+    methods: {
+     
+    }
+    
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+.search 
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    & input 
+        padding: 0
+        margin: 0
+        border: none
+    .search-input
+        box-sizing: border-box
+        width: 100%    
+        height: .62rem
+        padding: 0 .1rem
+        line-height: .62rem
+        text-align: center
+        border-radius: .06rem
+        color: #666
+</style>
+
